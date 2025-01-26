4cefdf74c213fafe53eca720af376c9698531c8b
v1pinx <vkprajapati665@gmail.com>
2025-01-26 17:55:21 +0530

    Update Server-side

--- server/model.js
+++ server/model.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-// Block Schema for "ANAGRAM" type questions
 const blockSchema = new Schema({
   text: { type: String, required: true },
   showInOption: { type: Boolean, default: true },
   isAnswer: { type: Boolean, default: false },
 });
 
-// Option Schema for "MCQ" type questions
 const optionSchema = new Schema({
   text: { type: String, required: true },
   isCorrectAnswer: { type: Boolean, required: true },
 });
 
-// Question Schema
-const questionSchema = new Schema({
-  type: { 
-    type: String, 
-    required: true, 
-    enum: ['ANAGRAM', 'MCQ', 'READ_ALONG', 'CONTENT_ONLY'],  // Added the new types here
+const questionSchema = new Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+      enum: ["ANAGRAM", "MCQ", "READ_ALONG", "CONTENT_ONLY"],
+    },
+    anagramType: {
+      type: String,
+      enum: ["WORD", "SENTENCE"],
+    },
+    blocks: [blockSchema],
+    options: [optionSchema],
+    solution: { type: String },
+    title: { type: String, required: true },
+    siblingId: { type: Schema.Types.ObjectId, ref: "Sibling", required: true },
   },
-  anagramType: { 
-    type: String, 
-    enum: ['WORD', 'SENTENCE'],
-  },
-  blocks: [blockSchema],  // Only for "ANAGRAM" type
-  options: [optionSchema],  // Only for "MCQ" type
-  solution: { type: String }, 
-  title: { type: String, required: true },
-  siblingId: { type: Schema.Types.ObjectId, ref: 'Sibling', required: true },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-const Question = mongoose.model('Question', questionSchema);
+const Question = mongoose.model("Question", questionSchema);
 
 module.exports = Question;
--- server/server.js
+++ server/server.js
@@ -1,76 +1,126 @@
-const gRPC = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
+const gRPC = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
 
-const packageDefinition = protoLoader.loadSync('./../proto/question.proto', {});
+const packageDefinition = protoLoader.loadSync("./proto/question.proto", {});
 const gRPCObject = gRPC.loadPackageDefinition(packageDefinition);
 
 const questionPackage = gRPCObject.question;
 
-const Question = require('./model');
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://vipindev665:bX0opTBpHPF9Qtz6@cluster0.jnbgy.mongodb.net').then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
+const Question = require("./model");
+require('dotenv').config();
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    process.env.MONGODB_URI
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
     console.error(error);
-} );
-
+  });
 
 async function GetAllQuestions(call, callback) {
+  const { page = 1, limit = 20 } = call.request;
+  const skip = (page - 1) * limit;
+
   try {
-    const questions = await Question.find({}).limit(20);
-    callback(null, { questions });
+    const totalCount = await Question.countDocuments();
+    const questions = await Question.find({}).skip(skip).limit(limit);
+    const totalPages = Math.ceil(totalCount / limit);
+    callback(null, { questions, totalPages, currentPage: page });
   } catch (err) {
     callback(err, null);
   }
-
 }
 
-async function GetQuestionByType(call, callback){
-  try{
+async function GetQuestionByType(call, callback) {
+  const { page = 1, limit = 20 } = call.request;
+  const skip = (page - 1) * limit;
+  try {
     const type = call.request.type;
-    const questionByType = await Question.find({ type: type }).limit(20);
-    callback(null, { questions: questionByType });
-  }
-  catch(err){
+    const totalCount = await Question.countDocuments({ type: type });
+    const questionByType = await Question.find({ type: type })
+      .skip(skip)
+      .limit(20);
+    const totalPages = Math.ceil(totalCount / limit);
+    callback(null, {
+      questions: questionByType,
+      totalPages,
+      currentPage: page,
+    });
+  } catch (err) {
     callback(err, null);
   }
 }
 
-async function GetQuestionBySearchString(call, callback){
-  try{
-    const searchString = call.request.searchString;
-    const questionBySearchString = await Question.find({ title: { $regex: searchString, $options: 'i' } }).limit(20);
-    callback(null, { questions: questionBySearchString });
-  }catch(err){
+async function GetQuestionBySearchString(call, callback) {
+  const { page = 1, limit = 20, searchString } = call.request;
+  const skip = (page - 1) * limit;
+
+  try {
+    const totalCount = await Question.countDocuments({
+      title: { $regex: searchString, $options: "i" },
+    });
+    const questionBySearchString = await Question.find({
+      title: { $regex: searchString, $options: "i" },
+    })
+      .skip(skip)
+      .limit(limit);
+    const totalPages = Math.ceil(totalCount / limit);
+    callback(null, {
+      questions: questionBySearchString,
+      totalPages,
+      currentPage: page,
+    });
+  } catch (err) {
     callback(err, null);
   }
 }
 
-async function GetQuestionByTypeAndSearchString(call, callback){
-  try{
-    const type = call.request.type;
-    const searchString = call.request.searchString;
-    const questionByTypeAndSearchString = await Question.find({ type: type, title: { $regex: searchString, $options: 'i' } }).limit(20);
-    callback(null, { questions: questionByTypeAndSearchString });
-  } catch(err){
+async function GetQuestionByTypeAndSearchString(call, callback) {
+  const { page = 1, limit = 20, type, searchString } = call.request;
+  const skip = (page - 1) * limit;
+
+  try {
+    const totalCount = await Question.countDocuments({
+      type,
+      title: { $regex: searchString, $options: "i" },
+    });
+    const questionByTypeAndSearchString = await Question.find({
+      type,
+      title: { $regex: searchString, $options: "i" },
+    })
+      .skip(skip)
+      .limit(limit);
+    const totalPages = Math.ceil(totalCount / limit);
+    callback(null, {
+      questions: questionByTypeAndSearchString,
+      totalPages,
+      currentPage: page,
+    });
+  } catch (err) {
     callback(err, null);
   }
 }
 
 const server = new gRPC.Server();
 server.addService(questionPackage.QuestionService.service, {
-    GetAllQuestions,
-    GetQuestionByType,
-    GetQuestionBySearchString,
-    GetQuestionByTypeAndSearchString
+  GetAllQuestions,
+  GetQuestionByType,
+  GetQuestionBySearchString,
+  GetQuestionByTypeAndSearchString,
 });
 
-server.bindAsync('0.0.0.0:9090', gRPC.ServerCredentials.createInsecure(), (error) => {
-    if(error) {
-        console.error(error);
-        return;
+server.bindAsync(
+  `0.0.0.0:${process.env.PORT}`,
+  gRPC.ServerCredentials.createInsecure(),
+  (error) => {
+    if (error) {
+      console.error(error);
+      return;
     }
-    console.log('Server is running on http://0.0.0.0:9090');
+    console.log(`Server is running on http://0.0.0.0:${process.env.PORT}`);
     server.start();
-})
+  }
+);
